ee432b588d5dc8d70f60a52bfeb591deedbfc6f2
Yousong Sun <sunyousong1991@hotmail.com>
2023-12-23 12:17:17 +0800

    next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,202 @@
-import Image from 'next/image'
+'use client'
+
+
+import React, { useState, useEffect } from 'react';
+
+
 
 export default function Home() {
+
+  const [selectedSize, setSelectedSize] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showCart, setShowCart] = useState(false);
+
+  interface Sizes {
+    S: number;
+    M: number;
+    L: number;
+  }
+
+  const [cartSizes, setCartSizes] = useState<Sizes>({ S: 0, M: 0, L: 0 });
+
+  const addToCart = (size: keyof Sizes) => {
+    setCartSizes(prevSizes => ({
+      ...prevSizes,
+      [size]: prevSizes[size] + 1
+    }));
+  };
+
+
+
+  const handleSizeClick = (size: any) => {
+    setSelectedSize(size);
+  };
+
+  const handleCartClick = () => {
+    setShowCart(!showCart);
+  };
+
+
+  useEffect(() => {
+    if (errorMessage) {
+      const timer = setTimeout(() => {
+        setErrorMessage('');
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [errorMessage]);
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-white container mx-auto">
+      <div className="bg-gray-100 h-6 flex items-center justify-end pr-6" >
+        <a className={`${showCart ? "bg-white border boder-1 boder-black" : "bg-gray-100"} h-6 flex items-center text-xs text-gray-600 px-1`}
+          onClick={() => handleCartClick()}
+        >
+          My cart ( {cartSizes.S + cartSizes.M + cartSizes.L} )
+        </a>
+      </div>
+      {showCart && (
+        <div className="absolute top-6 right-6 bg-white py-2 border">
+          <div className="w-[300px]">
+
+
+            {cartSizes.S > 0 && (<div className="flex p-4">
+              <div className="w-1/3">
+                <img src="/img/classic-tee.jpeg">
+                </img>
+              </div>
+              <div className="w-2/3 pl-4">
+                <p className="text-sm">Classic Tee</p>
+                <p className="pt-2 text-xs">{cartSizes.S}×  <span className="font-bold">$75.00</span></p>
+                <p className="pt-3 text-xs">Size: S</p>
+              </div>
+            </div>)}
+
+            {cartSizes.M > 0 && (<div className="flex p-4">
+              <div className="w-1/3">
+                <img src="/img/classic-tee.jpeg">
+                </img>
+              </div>
+              <div className="w-2/3 pl-4">
+                <p className="text-sm">Classic Tee</p>
+                <p className="pt-2 text-xs">{cartSizes.M}×  <span className="font-bold">$75.00</span></p>
+                <p className="pt-3 text-xs">Size: M</p>
+              </div>
+            </div>)}
+
+            {cartSizes.L > 0 && (<div className="flex p-4">
+              <div className="w-1/3">
+                <img src="/img/classic-tee.jpeg">
+                </img>
+              </div>
+              <div className="w-2/3 pl-4">
+                <p className="text-sm">Classic Tee</p>
+                <p className="pt-2 text-xs">{cartSizes.L}×  <span className="font-bold">$75.00</span></p>
+                <p className="pt-3 text-xs">Size: L</p>
+              </div>
+            </div>)}
+
+
+
+
+
+          </div>
+        </div>
+      )}
+
+
+
+      <div className="flex px-24 pt-10">
+        <div className="w-1/2 px-6">
+          <img src="/img/classic-tee.jpeg">
+          </img>
+        </div>
+        <div className="w-1/2 px-6">
+          <p className="text-xl">Classic Tie</p>
+          <p className="pt-6 font-bold text-sm">$75.00</p>
+          <p className="pt-6 text-xs text-gray-400">Dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Quod autem ratione actum est, id officium appellamus dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Quod autem ratione actum est, id officium appellamus.</p>
+          <p className="pt-6">
+            <span className="font-bold text-xs text-gray-400">SIZE</span><span className="text-red-500 text-xs">* </span>
+            <span className="text-xs font-bold">{selectedSize}</span>
+          </p>
+
+
+          <div className="flex pt-6">
+            <div
+              className={`w-10 h-10 border ${selectedSize === "S" ? "border-gray-800 border-2" : "border-gray-400 border-1"} flex items-center justify-center mr-2 text-xs`}
+              onClick={() => {
+                if (selectedSize !== "S") {
+                  handleSizeClick("S");
+                } else {
+                  handleSizeClick("");
+                }
+              }}
+            >
+              S
+            </div>
+            <div
+              className={`w-10 h-10 border ${selectedSize === "M" ? "border-gray-800 border-2" : "border-gray-400 border-1"} flex items-center justify-center mr-2 text-xs`}
+              onClick={() => {
+                if (selectedSize !== "M") {
+                  handleSizeClick("M");
+                } else {
+                  handleSizeClick("");
+                }
+              }}
+            >
+              M
+            </div>
+            <div
+              className={`w-10 h-10 border ${selectedSize === "L" ? "border-gray-800 border-2" : "border-gray-400 border-1"} flex items-center justify-center text-xs`}
+              onClick={() => {
+                if (selectedSize !== "L") {
+                  handleSizeClick("L");
+                } else {
+                  handleSizeClick("");
+                }
+              }}
+            >
+              L
+            </div>
+          </div>
+
+          <div className="flex pt-4">
+            <div className="w-36 h-9 border border-gray-800 border-2 flex items-center justify-center font-bold text-sm hover:bg-black hover:text-white select-none"
+              onClick={() => {
+                if (selectedSize !== "") {
+                  if (selectedSize == "S") {
+                    addToCart("S");
+                  } else if (selectedSize == "M") {
+                    addToCart("M");
+                  } else if (selectedSize == "L") {
+                    addToCart("L");
+                  }
+                } else {
+                  setErrorMessage('Choose Size First');
+                }
+              }}
+            >
+              ADD TO CART
+            </div>
+
+            {errorMessage && (
+              <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+                <div className="bg-red-500 text-white p-4 rounded shadow-lg">
+                  <p className="text-lg font-bold">{errorMessage}</p>
+                </div>
+              </div>
+            )}
+
+          </div>
+
+
+
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
